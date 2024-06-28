fe0bfbc7889a2dc5f3771b73d791283a44eb86ae
Serhii Krupnyk <sergiokrupnik@gmail.com>
2024-06-28 04:34:15 +0300

    init project, add some main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,155 @@
+"use client";
+import { Carousel, Navbar } from "flowbite-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main>
+      <div className="fixed flex flex-col items-center z-10">
+        <Navbar className="bg-[#271248] py-2.5 w-screen pl-2 pr-2 shadow-lg shadow-purple-900/50">
+          <Navbar.Brand as={Link} href="/">
+            <img
+              src="https://t4.ftcdn.net/jpg/05/30/89/65/360_F_530896520_khHbcG5ytp7gJcWiZG8lPLNbcCRAWjEI.webp"
+              className="mr-3 h-6 sm:h-12"
+              alt="Star Chance Casino Logo"
             />
-          </a>
+            <span className="self-center text-white whitespace-nowrap text-xl font-semibold dark:text-white">
+              Star Chance
+            </span>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <button
+              type="button"
+              className="text-white bg-gradient-to-r border border-blue-700 hover:bg-blue-800 from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Регистрация
+            </button>
+            <button
+              type="button"
+              className="text-white bg-gradient-to-r border border-blue-700 hover:bg-blue-800 from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Вход
+            </button>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="bg-[#361c60] h-[60px] p-2 w-3/5 flex flex-col items-center shadow-lg shadow-purple-900/50 rounded">
+          <div className="flex flex-row space-arround">
+            <button
+              type="button"
+              className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-white focus:outline-none bg-[#361c60] rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 uppercase"
+            >
+              слоты
+            </button>
+            <button
+              type="button"
+              className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-white focus:outline-none bg-[#361c60] rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 uppercase"
+            >
+              LIVE CASINO
+            </button>
+            <button
+              type="button"
+              className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-white focus:outline-none bg-[#361c60] rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 uppercase"
+            >
+              Настольные
+            </button>
+            <button
+              type="button"
+              className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-white focus:outline-none bg-[#361c60] rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 uppercase"
+            >
+              Правила Казино
+            </button>
+            <button
+              type="button"
+              className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-white focus:outline-none bg-[#361c60] rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 uppercase"
+            >
+              касса
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="relative pt-[79px]">
+        <div className="h-[480px] block">
+          <Carousel className="" slideInterval={2000} indicators={false} leftControl=" " rightControl=" ">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/005/525/156/non_2x/online-casino-banner-with-smartphone-shine-neon-casino-roulette-wheel-playing-cards-blue-neon-shine-slot-machine-and-poker-chips-in-dark-scene-with-hologram-of-digital-rings-vector.jpg"
+              alt="..."
+            />
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/003/332/122/original/pink-shine-neon-casino-roulette-wheel-with-playing-cards-vector.jpg"
+              alt="..."
+            />
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/027/010/338/non_2x/neon-casino-playing-cards-with-poker-chips-in-hologram-of-digital-rings-in-dark-empty-scene-vector.jpg"
+              alt="..."
+            />
+          </Carousel>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-row pl-2 pr-2 space-between pt-[20px]">
+        <div className="mx-auto flex flex-row items-center container bg-[#271248]">
+          <div className="w-4/6">
+            <div className="m-[30px] flex flex-col items-center">
+              <div className="flex flex-wrap w-full">
+                <div className="w-[250px] inline-block m-auto border-box">
+                  <img
+                    className="w-[250px] h-[250px] rounded-lg"
+                    src="https://media.pinkcasino.co.uk/images/games/sweet-bonanza/sweet-bonanza-square.jpg"
+                    alt=""
+                  />
+                  <p className="text-center text-lg text-white">Sweet Bonanza</p>
+                </div>
+                <div className="w-[250px] inline-block m-auto border-box">
+                  <img
+                    className="w-[250px] h-[250px] rounded-lg"
+                    src="https://media.pinkcasino.co.uk/images/games/sweet-bonanza/sweet-bonanza-square.jpg"
+                    alt=""
+                  />
+                  <p className="text-center text-lg text-white">Sweet Bonanza</p>
+                </div>
+                <div className="w-[250px] inline-block m-auto border-box">
+                  <img
+                    className="w-[250px] h-[250px] rounded-lg"
+                    src="https://media.pinkcasino.co.uk/images/games/sweet-bonanza/sweet-bonanza-square.jpg"
+                    alt=""
+                  />
+                  <p className="text-center text-lg text-white">Sweet Bonanza</p>
+                </div>
+                <div className="w-[250px] inline-block m-auto border-box">
+                  <img
+                    className="w-[250px] h-[250px] rounded-lg"
+                    src="https://media.pinkcasino.co.uk/images/games/sweet-bonanza/sweet-bonanza-square.jpg"
+                    alt=""
+                  />
+                  <p className="text-center text-lg text-white">Sweet Bonanza</p>
+                </div>
+                <div className="w-[250px] inline-block m-auto border-box">
+                  <img
+                    className="w-[250px] h-[250px] rounded-lg"
+                    src="https://media.pinkcasino.co.uk/images/games/sweet-bonanza/sweet-bonanza-square.jpg"
+                    alt=""
+                  />
+                  <p className="text-center text-lg text-white">Sweet Bonanza</p>
+                </div>
+                <div className="w-[250px] inline-block m-auto border-box">
+                  <img
+                    className="w-[250px] h-[250px] rounded-lg"
+                    src="https://media.pinkcasino.co.uk/images/games/sweet-bonanza/sweet-bonanza-square.jpg"
+                    alt=""
+                  />
+                  <p className="text-center text-lg text-white">Sweet Bonanza</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="h-[700px] min-h-[1em] w-0.5 self-stretch bg-neutral-100 dark:bg-white/10"></div>
+          <div className="w-2/6 h-[700px]">
+            <div className="m-[30px]">
+              <p className="text-center text-white uppercase text-lg">СЕЙЧАС ВЫИГРЫВАЮТ</p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,18 @@
 import type { Config } from "tailwindcss";
+const flowbite = require("flowbite-react/tailwind");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
+    "./node_modules/flowbite-react/lib/**/*.js",
+    flowbite.content(),
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), flowbite.plugin()],
 };
 export default config;
